6c162d7be8bacf322cff9570a8eb491fcaa36d45
Raja <r.rajaravi@gmail.com>
2016-01-26 12:43:48 +0530

    replaced dictionary of supported distros with named tuple

--- src/multi_distribution/__init__.py
+++ src/multi_distribution/__init__.py
@@ -1,13 +1,29 @@
 import os
+import collections
 
-supported_distros = [dict(name='redhat', rfile='/etc/redhat-release'),
-                         dict(name='solaris', rfile='/etc/release'),
-                         dict(name='ubuntu', rfile='/etc/lsb-release'),
-                         dict(name='suse', rfile='/etc/SuSE-release'),
-                    ]
+Distro = collections.namedtuple('Distro', 'name releaseFile') 
+
+# List to hold supported operating system distribution
+supported_distros = [
+    Distro('redhat', '/etc/redhat-release'),
+    Distro('solaris', '/etc/release'),
+    Distro('ubuntu', '/etc/lsb-release'),
+    Distro('suse', '/etc/SuSE-release'),
+]
 
 
 def get_valid_class(aclass, param='', value=''):
+    """ 
+    Check all the subclasses of a provided
+    class to see if the provided parameter 
+    and value exist in any of the subclass.
+  
+    Return the subclass if the parameter 
+    and value matches, else return the 
+    provided class itself.
+    """
+    
+    parameter
     param = param == '' and 'distro' or param
     value = value == '' and get_distro() or value
     _result = aclass
@@ -21,13 +37,29 @@ def get_valid_class(aclass, param='', value=''):
 
 
 def checkFile_and_return(adistro):
+    """
+    Check the provided distro to see
+    if the release file of the distro
+    exist in the system.
+
+    if release file exist, return the
+    distro, else return None
+    """
     try:
-        if os.path.isfile(adistro['rfile']):
+        if os.path.isfile(adistro.releaseFile):
             return adistro
     except IOError:
          return None
         
     
 def get_distro():
+    """
+    Using the list of supported distributions, check which
+    one matches with the current system.
+
+    Return the name of the matched distribution, else
+    return False
+    """
+
     valid_distro = filter(checkFile_and_return, supported_distros)
-    return len(valid_distro) > 0 and valid_distro[0]['name']
+    return len(valid_distro) > 0 and valid_distro[0].name
